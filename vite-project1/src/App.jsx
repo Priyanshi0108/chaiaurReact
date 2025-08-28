435d7ff7a60f16c7d31d74944a7c08aa253f4d5e
Priyanshi0108 <singhamanrajpoot2007@gmail.com>
2025-08-28 22:09:29 +0530

    next change

--- vite-project1/src/App.jsx
+++ vite-project1/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import Chai from './chai.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-      <h1>Chai aur React with fun</h1>
+      <h1>Chai aur React with fun||Vite</h1>
+      <Chai/>
     </>
   )
 }
 
-export default App
+export default App;
