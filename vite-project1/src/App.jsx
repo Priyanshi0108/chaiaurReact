3036c113d4000c872d2434fe63fcfd5645ac814d
Priyanshi0108 <singhamanrajpoot2007@gmail.com>
2025-08-28 21:32:16 +0530

    add React

---
+++ vite-project1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Chai aur React with fun</h1>
+    </>
+  )
+}
+
+export default App
