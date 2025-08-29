2305cad5ec651ad33e196aded462af07ff5c7077
Priyanshi0108 <singhamanrajpoot2007@gmail.com>
2025-08-29 08:31:45 +0530

    Add Vite2

---
+++ vite-project2/src/card.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Card(){
+    return(
+        <div class="flex flex-col items-center gap-6 p-7 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="/img/cover.png" />
+  </div>
+  <div class="flex items-center">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+
+    )
+}
+export default Card;
